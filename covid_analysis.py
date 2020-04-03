3408599f6e5e411c295ca50da14d71f3f15a80db
Kyle Sower <kylesower@gmail.com>
2020-04-03 14:05:52 -0600

    Added some functionality to automatically remove outliers

--- covid_analysis.py
+++ covid_analysis.py
@@ -48,13 +48,37 @@ if use_states:
 else:
     pop = pd.read_csv('countrypop.csv',index_col = 'CountryName')
 
+# If you want to limit your analysis to states over a certain threshold of dead/cases,
+# uncomment the section below
+#---------------
+if fit_deaths:
+    for state in df.T.index:
+        if df.T.loc[state].max()<0:
+            print(state)
+            df = df.T.drop(state).T
+else:
+    for state in df.T.index:
+        if use_states:
+            if df.T.loc[state].max()<200:
+                print(state)
+                df = df.T.drop(state).T
+        else:
+            if df.T.loc[state].max()<1000:
+                print(state)
+                df = df.T.drop(state).T
+#---------------
+
 if use_states:
-    outliers = ['Virgin Islands', 'Northern Mariana Islands', 'American Samoa', \
+    outliers = []
+    '''['Virgin Islands', 'Northern Mariana Islands', 'American Samoa', \
                 'Rhode Island', 'New Mexico', 'New Hampshire', 'Nebraska', \
                 'Louisiana', 'Hawaii', 'Grand Princess', 'Diamond Princess', \
-                 'Arkansas', 'Guam']
+                'Arkansas', 'Guam']
+    '''
+
 else:
-    outliers = ['Afghanistan', 'Albania', 'Andorra', 'Angola', 'Antigua and Barbuda', \
+    outliers = ['Russia']
+    '''['Afghanistan', 'Albania', 'Andorra', 'Angola', 'Antigua and Barbuda', \
                 'Argentina', 'Armenia', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', \
                 'Belarus', 'Belize', 'Benin', 'Bhutan', 'Bolivia', 'Botswana', 'Brazil', 'Brunei', \
                 'Bulgaria', 'Burma', 'Burundi', 'Cabo Verde', 'Cambodia', 'Cameroon', \
@@ -78,24 +102,12 @@ else:
                 'Togo', 'Trinidad and Tobago', 'Uganda', 'United Arab Emirates', 'Uruguay', \
                 'Venezuela', 'West Bank and Gaza', 'Zambia', 'Zimbabwe'
                 ]
+    '''
    
 # Remove outliers that either had bad fits or not enough data 
 df = df.T.drop(outliers).T
 
-# If you want to limit your analysis to states under a certain threshold of dead/cases,
-# uncomment the section below
-'''
-for state in dead.T.index:
-    print(state)
-    if dead.loc['4/1/20'][state]>50:
-        print(state)
-        dead = dead.T.drop(state).T
-for state in conf.T.index:
-    print(state)
-    if conf.loc['4/1/20'][state]>2000:
-        print(state)
-        conf = conf.T.drop(state).T
-'''
+
 
 #plt.plot(conf)
 #plt.xticks([5*i for i in range(6)])
@@ -106,24 +118,29 @@ for i, state in enumerate(df.T.index):
         if use_states:
             popt, pcov = cf(fit,np.arange(0,len(df.T.loc[state])),df.T.loc[state],p0=[2*df.T.loc[state].max(),0.3,20])
         else:
-            popt, pcov = cf(fit,np.arange(0,len(df.T.loc[state])),df.T.loc[state],p0=[2*df.T.loc[state].max(),0.3,20+date_sub])
+            popt, pcov = cf(fit,np.arange(0,len(df.T.loc[state])),df.T.loc[state],p0=[2*df.T.loc[state].max(),0.3,30+date_sub])
     except:
         print(state)
 
     y = fit(np.arange(0,len(df.T.loc[state])),*popt)
-    plt.plot(df.loc[:][state],label='data')
-    plt.plot(y,label='fit')
-    plt.xticks([5*i for i in range(6)])
-    plt.title(state)
-    if fit_deaths:
-        plt.ylabel('Dead')
-    else:
-        plt.ylabel('Confirmed COVID-19 Cases')
-    plt.xlabel('Date')
-    plt.legend()
-    plt.figure()
+    SSR_norm = ((np.array(df.T.loc[state])-y)**2).sum()/df.T.loc[state].max()
+    print('state: %s, SSR: %f' %(state, SSR_norm))
     
     rates[state]=popt
+    if SSR_norm > 900:
+        del rates[state]
+    else:
+        plt.plot(df.loc[:][state],label='data')
+        plt.plot(y,label='fit')
+        plt.xticks([5*i for i in range(6)])
+        plt.title(state)
+        if fit_deaths:
+            plt.ylabel('Dead')
+        else:
+            plt.ylabel('Confirmed COVID-19 Cases')
+        plt.xlabel('Date')
+        plt.legend()
+        plt.figure()
 
 if use_states:
     z = [rates[state][0]/pop.loc[state].max()*1e6 for state in rates]
